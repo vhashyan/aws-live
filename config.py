46242d2fad66e947d4d54eeb2095ec84a0d64254
vhashyan <109597174+vhashyan@users.noreply.github.com>
2022-09-05 09:12:10 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cakqpm4v0e15.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "vhashyan-employee"
+customregion = "us-east-1"
